7a74dfcf7216d19ceaedd8682e5be6c03b5cfe49
popcorn150 <hungrymanchannel@gmail.com>
2024-11-27 17:44:26 +0100

    Ts advanced shopping cart

--- Ts Advanced/src/context/shoppingCartContext.tsx
+++ Ts Advanced/src/context/shoppingCartContext.tsx
@@ -1,4 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type ShoppingCartProviderProps = {
     children: ReactNode
@@ -29,7 +31,7 @@ export function useShoppingCart() {
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
     const [isOpen, setIsOpen] = useState(false)
-    const [cartItems, setCartItems] = useState<CartItem[]>([])
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart", [])
 
     const cartQuantity = cartItems.reduce((quantity, item) =>
         item.quantity + quantity, 0
@@ -92,5 +94,6 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
         }}
     >
         {children}
+        <ShoppingCart isOpen={isOpen} />
     </ShoppingCartContext.Provider>
 }
